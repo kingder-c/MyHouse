91800f53390173c32c08edd2206713a77c6d322f
Kingder <zj809667533@live.com>
2018-10-15 00:19:17 +0800

    test

--- app/router.js
+++ app/router.js
@@ -7,3 +7,7 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
 };
+module.exports = getHouseByTime => {
+  const { router, controller } = getHouseByTime;
+  router.get('/getHouseByTime', controller.home.index);
+};
