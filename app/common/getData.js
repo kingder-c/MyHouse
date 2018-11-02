a1c91f3f675af789d329b9bd778cc623ab43b546
Kingder <zj809667533@live.com>
2018-11-03 00:54:31 +0800

    dingshirenwu

---
+++ app/common/getData.js
@@ -0,0 +1,75 @@
+// import { resolve } from 'url';
+
+'use strict';
+
+const getHtml = require('../common/unit');
+const load58Info = require('../common/formater');
+const mongoose = require('mongoose');
+
+class HomeController {
+  async getHouseInfo() {
+    const baseUrl = 'https://wf.58.com/';
+    const List = [ 'kuiwen/ershoufang/pn', 'wfgaoxinqu/ershoufang/pn', 'weicheng/ershoufang/pn', 'hanting/ershoufang/pn' ];
+    const houseInfo = [];
+    let waitFlag = false;
+    for (let i = 0; i < List.length; i++) {
+      const areaUrl = baseUrl + List[i];
+      for (let j = 1; j <= 70; j++) {
+        if (waitFlag) {
+          waitFlag = !waitFlag;
+          j--;
+        }
+        const pageUrl = areaUrl + j + '/';
+        let html = '';
+        await getHtml(pageUrl).then(res => {
+          html = res;
+        });
+        if (load58Info(html).length === 0) {
+          await this.wait();
+          waitFlag = true;
+        }
+        console.log(i, j);
+        console.log(load58Info(html).length);
+        houseInfo.push(...load58Info(html));
+      }
+    }
+    // this.ctx.body = JSON.stringify(houseInfo);
+    mongoose.connect('mongodb://106.12.144.76:27017/local', function(err) {
+      if (err) {
+        console.log(err);
+      }
+      if (!err) {
+        const schema = new mongoose.Schema({
+          title: String,
+          type: String,
+          area: Number,
+          origined: String,
+          layer: String,
+          community: String,
+          district: String,
+          address: String,
+          allPrice: Number,
+          unitPrice: Number,
+          date: String,
+        });
+        const housecollect = mongoose.model('housecollect', schema);
+        housecollect.create(...houseInfo, function(err) {
+          console.log('共录入' + houseInfo.length + '条');
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    });
+
+  }
+  async wait() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(1);
+      }, 90000);
+    });
+  }
+}
+
+module.exports = HomeController;
--- app/schedule/getHouseData.js
+++ app/schedule/getHouseData.js
@@ -1,20 +1,27 @@
 'use strict';
 
 const Subscription = require('egg').Subscription;
-const getHouseInfo = require('../controller/pushInfo').getHouseInfo;
+// const getHouseInfo = require('../controller/pushInfo');
+const getDate = require('../common/getData');
 
 class getHouseData extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
+  // constructor() {
+  //   console.log(getHouseInfo);
+  // }
   static get schedule() {
     return {
-      interval: (60 * 6 + 'm'), // 1 分钟间隔
+      interval: (60 * 24 + 'm'), // 1 分钟间隔
+      // interval: ('1m'), // 1 分钟间隔
       type: 'all', // 指定所有的 worker 都需要执行
     };
   }
 
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    await getHouseInfo();
+    const a = new getDate();
+    // console.log(a.getHouseInfo);
+    await a.getHouseInfo();
   }
 }
 
