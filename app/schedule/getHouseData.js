fd8742ac687d8080f33357cb015dc5277e5b6b7d
Kingder <zj809667533@live.com>
2018-11-03 19:32:06 +0800

    去掉定时任务

--- app/schedule/getHouseData.js
+++ app/schedule/getHouseData.js
@@ -1,27 +1,27 @@
-'use strict';
+// 'use strict';
 
-const Subscription = require('egg').Subscription;
-// const getHouseInfo = require('../controller/pushInfo');
-const getDate = require('../common/getData');
+// const Subscription = require('egg').Subscription;
+// // const getHouseInfo = require('../controller/pushInfo');
+// const getDate = require('../common/getData');
 
-class getHouseData extends Subscription {
-  // 通过 schedule 属性来设置定时任务的执行间隔等配置
-  // constructor() {
-  //   console.log(getHouseInfo);
-  // }
-  static get schedule() {
-    return {
-      interval: (60 * 24 + 'm'), // 1 分钟间隔
-      // interval: ('1m'), // 1 分钟间隔
-      type: 'all', // 指定所有的 worker 都需要执行
-    };
-  }
+// class getHouseData extends Subscription {
+//   // 通过 schedule 属性来设置定时任务的执行间隔等配置
+//   // constructor() {
+//   //   console.log(getHouseInfo);
+//   // }
+//   static get schedule() {
+//     return {
+//       interval: (60 * 24 + 'm'), // 1 分钟间隔
+//       // interval: ('1m'), // 1 分钟间隔
+//       type: 'all', // 指定所有的 worker 都需要执行
+//     };
+//   }
 
-  // subscribe 是真正定时任务执行时被运行的函数
-  async subscribe() {
-    const a = new getDate();
-    await a.getHouseInfo();
-  }
-}
+//   // subscribe 是真正定时任务执行时被运行的函数
+//   async subscribe() {
+//     const a = new getDate();
+//     await a.getHouseInfo();
+//   }
+// }
 
-module.exports = getHouseData;
+// module.exports = getHouseData;
